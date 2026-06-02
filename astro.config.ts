18e9e518cadef83a9701ef3fd525f709d1180453
wyclab <wyc1996@gmail.com>
2026-06-02 23:18:48 +0800

    new

--- astro.config.ts
+++ astro.config.ts
@@ -25,7 +25,7 @@ import config from './src/site.config.ts'
 // https://astro.build/config
 export default defineConfig({
   // [Basic]
-  site: 'https://lab.wyc.cc',
+  site: 'https://wyclab.com',
   // Deploy to a sub path
   // https://astro-pure.js.org/docs/setup/deployment#platform-with-base-path
   // base: '/astro-pure/',
