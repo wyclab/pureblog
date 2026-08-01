54b2a505f464f43e6c36c6467a40916e0b25930b
wuyucun <wyc1996@gmail.com>
2026-08-01 10:47:04 +0800

    修复搜索功能

--- src/site.config.ts
+++ src/site.config.ts
@@ -129,19 +129,19 @@ export const integ: IntegrationUserConfig = {
   // Add a random quote to the footer (default on homepage footer)
   // See: https://astro-pure.js.org/docs/integrations/advanced#web-content-render
   // [Quote]
-  //quote: {
-  // - Hitokoto
-  // https://developer.hitokoto.cn/sentence/#%E8%AF%B7%E6%B1%82%E5%9C%B0%E5%9D%80
-  // server: 'https://v1.hitokoto.cn/?c=i',
-  // target: `(data) => (data.hitokoto || 'Error')`
-  // - Quotable
-  // https://github.com/lukePeavey/quotable
-  // server: 'http://api.quotable.io/quotes/random?maxLength=60',
-  // target: `(data) => data[0].content || 'Error'`
-  // - DummyJSON
-  //  server: 'https://dummyjson.com/quotes/random',
-  //  target: `(data) => (data.quote.length > 80 ? \`\${data.quote.slice(0, 80)}...\` : data.quote || 'Error')`
-},
+  quote: {
+    // - Hitokoto
+    // https://developer.hitokoto.cn/sentence/#%E8%AF%B7%E6%B1%82%E5%9C%B0%E5%9D%80
+    server: 'https://v1.hitokoto.cn/?c=i',
+    target: `(data) => (data.hitokoto || 'Error')`
+    // - Quotable
+    // https://github.com/lukePeavey/quotable
+    // server: 'http://api.quotable.io/quotes/random?maxLength=60',
+    // target: `(data) => data[0].content || 'Error'`
+    // - DummyJSON
+    //  server: 'https://dummyjson.com/quotes/random',
+    //  target: `(data) => (data.quote.length > 80 ? \`\${data.quote.slice(0, 80)}...\` : data.quote || 'Error')`
+  },
   // [Typography]
   // https://unocss.dev/presets/typography
   typography: {
